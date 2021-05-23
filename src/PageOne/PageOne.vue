92640cc6c63321d6123cc7cdbe0e5cef7cf39e7d
Adam Barnhard <Adam.Barnhard@vizio.com>
2021-05-22 23:03:38 -0600

    Added singleton module

--- src/PageOne/PageOne.vue
+++ src/PageOne/PageOne.vue
@@ -2,10 +2,12 @@
   <div>
     <div>{{ staticVersion }}</div>
     <div>{{ dynamicVersion }}</div>
+    <div>{{ singletonDynamicVersion }}</div>
   </div>
 </template>
 
 <script>
+import singletonVersionUtil from '@/Shared/singletonVersionUtil'
 import VersionUtil from '@/Shared/versionUtil'
 const versionUtil = new VersionUtil()
 
@@ -13,8 +15,8 @@ export default {
   name: 'PageOne',
   data() {
     return {
-      versionUtil: {},
-      serverVersion: 'N/A'
+      serverVersion: 'N/A',
+      singletonServerVersion: 'N/A'
     }
   },
   computed: {
@@ -22,12 +24,19 @@ export default {
       return `Page One hardcoded version: ${versionUtil.version}`
     },
     dynamicVersion() {
-      return `Page One dynamic version: ${this.serverVersion}`
+      return `Page One server version: ${this.serverVersion}`
+    },
+    singletonDynamicVersion() {
+      return `Page One server singleton version: ${this.singletonServerVersion}`
     }
   },
   mounted() {
     versionUtil.serverPromise.then((data) => {
       this.serverVersion = data.serverVersion
+    })
+
+    singletonVersionUtil.serverPromise.then((data) => {
+      this.singletonServerVersion = data.serverVersion
     })
   }
 }
---
+++ src/Shared/singletonVersionUtil.js
@@ -0,0 +1,17 @@
+const hardCodedVersion = 1
+
+class VersionUtil {
+  constructor() {
+    this.version = hardCodedVersion
+    this.serverPromise = new Promise((resolve, reject) => {
+      fetch('/server-version.json')
+      .then((response) => response.json())
+      .then((data) => {
+        resolve(data)
+      })
+      .catch(reject)
+    })
+  }
+}
+
+export default new VersionUtil()
