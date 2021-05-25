a3eb8b8a7efae325c44f7c27e0962f2e73b85edc
Adam Barnhard <Adam.Barnhard@vizio.com>
2021-05-25 13:14:59 -0600

    Added home page version

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="hello">
     <h1>Home Page</h1>
+    <table>
+      <tbody>
+        <tr><td>Component Version</td><td>{{internalVersion}}</td></tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -9,24 +14,32 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      internalVersion: 1
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+  table {
+    margin: auto;
+  }
 </style>
